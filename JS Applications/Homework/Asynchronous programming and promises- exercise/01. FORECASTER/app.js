f56f20b90c0ace0ae4f881136bde3ffb12ab0e76
Delyan Dimitrov <dimitrovdelyann@gmail.com>
2020-03-25 14:07:50 +0200

    asynchronous programming exercise

--- JS Applications/Homework/Asynchronous programming and promises- exercise/01. FORECASTER/app.js
+++ JS Applications/Homework/Asynchronous programming and promises- exercise/01. FORECASTER/app.js
@@ -1,5 +1,106 @@
 function attachEvents() {
-    console.log("TODO...");
+    let submitButton=document.getElementById('submit');
+    let inputField=document.getElementById('location');
+    let forecastPart=document.getElementById('forecast');
+    let current=document.getElementById('current');
+    let upcoming=document.getElementById('upcoming');
+
+    let notificationBar=document.getElementById('notf');
+
+    let symbols={
+        'Sunny': '☀',
+        'Partly sunny': "⛅",
+        'Overcast': '☁',
+        'Rain': '☂',
+        'Degrees': '°',
+    }
+
+    const errorHandler=(e)=>{
+        console.dir(e);
+        notificationBar.innerHTML='Error ocurred!';
+    }
+
+        submitButton.addEventListener('click',async()=>{
+            fetch(`https://judgetests.firebaseio.com/locations.json`)
+            .then(x=>x.json())
+            .then(x=>{
+                let code=x.find(x=>x.name===inputField.value).code;
+                forecastPart.style.display='block';
+
+                Promise.all([
+                    fetch(`https://judgetests.firebaseio.com/forecast/today/${code}.json`).then(x=>x.json()),
+                    fetch(`https://judgetests.firebaseio.com/forecast/upcoming/${code}.json`).then(x=>x.json())
+                ])
+                .then(showWheatherLocation)            
+                .catch(errorHandler);                
+               
+            })
+            .catch(errorHandler);
+        });
+
+        function showWheatherLocation([todayData,upcomingData]){
+            const {condition,high,low}=todayData.forecast;
+        
+
+            let forecastDiv=createHTMLElement('div',['forecasts']);
+
+            let symbolSpam=createHTMLElement('spam',['condition','symbol'],symbols[condition]);
+
+            let conditionSpan=createHTMLElement('span',['condition']);
+
+            let deg=`${low}${symbols.Degrees}/${high}${symbols.Degrees}`;
+            let forecaseFirst=createHTMLElement('span',['forecast-data'],todayData.name);
+            let forecaseSecond=createHTMLElement('span',['forecast-data'],deg);
+            let forecaseThird=createHTMLElement('span',['forecast-data'],condition);
+
+            conditionSpan.appendChild(forecaseFirst);
+            conditionSpan.appendChild(forecaseSecond);
+            conditionSpan.appendChild(forecaseThird);
+
+            forecastDiv.appendChild(symbolSpam);
+            forecastDiv.appendChild(conditionSpan);
+
+            current.appendChild(forecastDiv);
+
+
+            const{forecast,name}=upcomingData;
+            let forecastInfoDiv=createHTMLElement('div',['forecast-info']);
+
+            forecast.forEach(({condition,high,low})=>{
+                console.log(condition);
+                console.log(high);
+                console.log(low);
+                let upcomingSpan=createHTMLElement('span',['upcoming']);
+
+                let deg=`${low}${symbols.Degrees}/${high}${symbols.Degrees}`;
+                let symbolSpan=createHTMLElement('span',['symbol'],symbols[condition]);
+                let degreesSpan=createHTMLElement('span',['forecast-data'],deg);
+                let conditionSpan=createHTMLElement('span',['forecast-data'],condition);
+            
+                upcomingSpan.appendChild(symbolSpan);
+                upcomingSpan.appendChild(degreesSpan);
+                upcomingSpan.appendChild(conditionSpan);
+
+                forecastInfoDiv.appendChild(upcomingSpan);
+            });
+
+            upcoming.appendChild(forecastInfoDiv);
+        }
+
+        function createHTMLElement(tagName,classNames,textContent){
+            let element=document.createElement(tagName);
+        
+            if(classNames){
+                element.classList.add(...classNames);
+            }
+        
+            if(textContent){
+                element.textContent=textContent;
+            }
+        
+            return element;
+        }
+    
 }
 
 attachEvents();
--- JS Applications/Homework/Asynchronous programming and promises- exercise/02. FISHER-GAME/app.js
+++ JS Applications/Homework/Asynchronous programming and promises- exercise/02. FISHER-GAME/app.js
@@ -1,5 +1,62 @@
 function attachEvents() {
-    console.log('TODO...');
+
+    const elements={
+        anglerInput:document.querySelector('#addForm input.angler'),
+        weightInput:document.querySelector('#addForm input.weight'),
+        speciesInput:document.querySelector('#addForm input.species'),
+        locationInput:document.querySelector('#addForm input.location'),
+        baitInput:document.querySelector('#addForm input.bait'),
+        captureTimeInput:document.querySelector('#addForm input.captureTime'),
+
+        addButton:document.getElementsByClassName('add')[0],
+        deleteButton:document.getElementsByClassName('delete')[0],
+        updateButton:document.getElementsByClassName('update')[0],
+        loadButton:document.getElementsByClassName('load')[0],
+    }
+
+    elements.addButton.addEventListener('click',async ()=>{
+        let myCatch={
+            angler: elements.anglerInput.value,
+            weight: elements.weightInput.value,
+            species: elements.speciesInput.value,
+            location: elements.locationInput.value,
+            bait: elements.baitInput.value,
+            capture: elements.captureTimeInput.value,
+        };
+
+        try{
+            const response = await fetch(`https://fisher-game.firebaseio.com/catches.json`,{
+            method: "Post",
+            headers:{
+                'Content-type':'application/json'
+            },
+            body: JSON.stringify(myCatch)
+        });
+
+            const data=await response.json();
+        }catch(error){
+            console.log(error);
+        }
+    });
+
+    elements.deleteButton.addEventListener('click',async ()=>{
+        const catchId="-M3DM--UOC3yxmlUNxxh";
+
+        try{
+           const response=await fetch(`https://fisher-game.firebaseio.com/catches/${catchId}.json`,{
+                headers:{
+                    'Content-Type':'application/json'
+                },
+                method: 'DELETE'
+            }); //return promise
+
+            const data=await response.json();
+            console.log(data); //return null
+
+        }catch(error){
+            console.log(error);
+        }
+    })
 }
 
 attachEvents();
