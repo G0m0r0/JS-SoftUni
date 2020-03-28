3e165c4883a423c31086a9a29ab066ca8f32200f
Delyan Dimitrov <dimitrovdelyann@gmail.com>
2020-03-28 12:31:19 +0200

    setup routing project

---
+++ JS Applications/Homework/Routing- lab/HTML-Skeleton/Resources/app.js
@@ -0,0 +1,8 @@
+(()=>{
+    let firstInput=document.getElementById('new-make');
+    firstInput.addEventListener('click',(e)=>{
+        e.preventDefault();
+        console.log('hi');
+    })
+    console.log('hi');
+})();
